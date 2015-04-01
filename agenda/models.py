6d57cbb2f639a594b3c98a740e49dcc0ac22fcb1
Joanna Sendorek <send.joanna@gmail.com>
2015-04-01 13:51:13 +0200

    slightly improve models and views

--- agenda/models.py
+++ agenda/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class Event(models.Model):
+    date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
     title = models.CharField(max_length=150)
     prelector = models.CharField(max_length=50)
     classroom = models.CharField(max_length=3)
@@ -16,6 +16,11 @@ class Event(models.Model):
     def __str__(self):
         return unicode(self)
 
+    def getStartTime(self):
+        return self.start_time.hour + self.start_time.minute
+
+    #def getEndTime(self):
+     #   return self.end_time.hour + self.end_time.minute
 
 class Type(models.Model):
     pass
--- agenda/views.py
+++ agenda/views.py
@@ -5,7 +5,7 @@ from agenda import models
 
 
 def index(request):
-    all_events = list(models.Event.objects.order_by('-start_time'))
+    all_events = list(models.Event.objects.order_by('-date', '-start_time'))
     context = {'events': all_events}
     response = TemplateResponse(request, 'index.html', context)
     return response
